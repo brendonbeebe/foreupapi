3378215c6149a6f38106ee3427276d8caa3a14e8
brendonbeebe <brendon.beebe@gmail.com>
2017-02-21 17:52:20 -0700

    Tests passing now

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -3,3 +3,6 @@ var before = require('hooks').before;
 before("Tee Sheets > Bookings > Create One", function (transaction) {
   transaction.skip = true;
 });
+before("Tee Sheets > Bookings > Delete One", function (transaction) {
+    transaction.skip = true;
+});
